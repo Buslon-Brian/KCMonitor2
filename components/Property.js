cc2797eaece6d762d8573948597d348ac098528e
Buslon-Brian <bbuslon@nevada.unr.edu>
2023-11-04 19:00:53 -0700

    added selector for buttons to property rounds

--- components/Property.js
+++ components/Property.js
@@ -1,10 +1,13 @@
 import {View, Text, TouchableOpacity} from 'react-native';
 import { SafeAreaProvider } from "react-native-safe-area-context"
 import styles from "./styles"
+import { useState } from 'react';
 
 
 export default function Property({navigation}){
-
+    const [tab, setTab] = useState("scanned")
+   
+   
     return(
         <SafeAreaProvider>
 
@@ -12,12 +15,10 @@ export default function Property({navigation}){
                     <Text style={{ color: 'white', fontSize: 32, marginLeft: "3%" }}> Property Rounds </Text>
             </View>
 
-            <View style = {{backgroundColor: '#25292e', flex: 1, flexDirection: 'column'}}>
-                <View style = {{backgroundColor: '#333940', flex: .15, flexDirection: 'row'}}>
-                    <TouchableOpacity style = {{backgroundColor: 'red', flex: .5, margin: '1%'}}></TouchableOpacity>
-                    <TouchableOpacity style = {{backgroundColor: 'blue', flex: .5, margin: '1%'}}></TouchableOpacity>
-                </View>
+            <View style={{ backgroundColor: '#25292e', flex: 1, flexDirection: 'column' }}>
+                {Selector(setTab)}
             </View>
+
 
             <TouchableOpacity
                 style={{ backgroundColor: "#333940", flex: .09, alignItems: 'center', justifyContent: 'center' }}
@@ -27,5 +28,25 @@ export default function Property({navigation}){
 
         </SafeAreaProvider>
     )
+}
+
+
+function Selector(setTab) {
+    return <View style={{ backgroundColor: '#25292e', flex: .15, flexDirection: 'row' }}>
+
+        <TouchableOpacity
+            style={{ backgroundColor: '#333940', flex: .5, margin: '1%', alignItems: 'center', justifyContent: 'center' }}
+            onPress={() => setTab("unscanned")}
+        >
+            <Text style={{ color: 'white', fontSize: 25 }}> Unscanned Codes</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+            style={{ backgroundColor: '#333940', flex: .5, margin: '1%', alignItems: 'center', justifyContent: 'center' }}
+            onPress={() => setTab("scanned")}
+        >
+            <Text style={{ color: 'white', fontSize: 25 }}> Scanned Codes</Text>
+        </TouchableOpacity>
+    </View>;
 }
 
