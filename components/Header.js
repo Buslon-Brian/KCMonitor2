dd8bea20687b40a100c6217c37d647282aeb72a1
Buslon-Brian <bbuslon@nevada.unr.edu>
2024-01-24 12:42:44 -0800

    Fixed Menu Images

--- components/Header.js
+++ components/Header.js
@@ -3,16 +3,16 @@ import styles from './styles'
 
 export default function Header_c({title, navigation}) {
     return (
-        <View style={[styles.header_bar, {flexDirection: 'row', alignItems: 'center'}]}>
+        <View style={[styles.header_bar, {flexDirection: 'row', alignItems: 'center',}]}>
             <TouchableOpacity 
                 style = {{flex: .05, alignItems: 'center'}}
                 onPress={()=> navigation.navigate('Menu')}
             >
-                <Image style = {{width: 33, height: 33, marginLeft: 5}} source = {require('../assets/images/back_arrow2.png')}/>
+                <Image style = {{width: 33, height: 33, marginLeft: 25}} source = {require('../assets/images/back_arrow2.png')}/>
 
             </TouchableOpacity>
             
-            <Text style = {{ flex: 1, color: 'white', fontSize: 32,}}> {title} </Text>
+            <Text style = {{ flex: 1, color: 'white', fontSize: 32, marginLeft: 25}}> {title} </Text>
             
             <TouchableOpacity 
                 style = {{flex: .2, alignItems: 'flex-end'}}
--- routes/MenuScreen.js
+++ routes/MenuScreen.js
@@ -3,7 +3,14 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { useSnapshot } from 'valtio';
 import { store } from '../stores/store';
 import styles from '../components/styles';
-
+import peopleImage from '../assets/images/people.png';
+import qrCodeImage from '../assets/images/QRCode.png';
+import buildingImage from '../assets/images/Building.png';
+import clockImage from '../assets/images/Clock.png';
+import printerImage from '../assets/images/Printer.png';
+import keyImage from '../assets/images/Key.png';
+import ticketsImage from '../assets/images/Tickets.png';
+import logoutImage from '../assets/images/LogOut.png';
 
 function MenuScreen({ navigation }) {
   const snap =useSnapshot(store)
@@ -22,92 +29,73 @@ function MenuScreen({ navigation }) {
 
 const DATA = [
   {
-   id: '1',
-   path: "../assets/images/people.png",
-   title: 'Headcounts',
-   screen: 'Headcounts',
+    id: '1',
+    path: peopleImage,
+    title: 'Headcounts',
+    screen: 'Headcounts',
   },
-
   {
-   id: '2',
-   path: "../assets/images/QRCode.png",
-   title: 'QR Codes',
-   screen: 'QR',
+    id: '2',
+    path: qrCodeImage,
+    title: 'QR Codes',
+    screen: 'QR',
   },
-
   {
-   id: '3',
-   path: "../assets/images/Building.png",
-   title: 'Property Rounds',
-   screen: 'Property'
+    id: '3',
+    path: buildingImage,
+    title: 'Property Rounds',
+    screen: 'Property',
   },
-
   {
-   id: '4',
-   path: "../assets/images/Clock.png",
-   title: 'Hourly Assignments',
-   screen: 'Hourly'
+    id: '4',
+    path: clockImage,
+    title: 'Hourly Assignments',
+    screen: 'Hourly',
   },
-
   {
-   id: '5',
-   path: "../assets/images/Printer.png",
-   title: 'Printers',
-   screen: 'Printers'
+    id: '5',
+    path: printerImage,
+    title: 'Printers',
+    screen: 'Printers',
   },
-
   {
-   id: '6',
-   path: "../assets/images/Key.png",
-   title: 'Keys',
-   screen: 'Keys'
+    id: '6',
+    path: keyImage,
+    title: 'Keys',
+    screen: 'Keys',
   },
-
   {
-   id: '7',
-   path: "../assets/images/Tickets.png",
-   title: 'Maintenance Tickets',
+    id: '7',
+    path: ticketsImage,
+    title: 'Maintenance Tickets',
   },
-
   {
-   id: '9',
-   path: "../assets/images/LogOut.png",
-   title: 'Log Out',
-   screen: 'Login'
+    id: '9',
+    path: logoutImage,
+    title: 'Log Out',
+    screen: 'Login',
   },
-
 ];
  
-const Item = ({title, path, screen, navigation}) => (
- <TouchableOpacity 
-   style={styles.item} 
-   onPress={() => {
-    if (title == 'Maintenance Tickets') {
-      Linking.openURL('https://unr.teamdynamix.com/TDClient/Requests/ServiceDet?ID=27661').catch((err) =>
-        console.error('Error opening URL:', err)
-      );
-    } else {
-      // Navigate to screen if no URL
-      navigation.navigate(screen);
-    }
-  }}
+const Item = ({ title, path, screen, navigation }) => (
+  <TouchableOpacity
+    style={styles.item}
+    onPress={() => {
+      if (title === 'Maintenance Tickets') {
+        Linking.openURL('https://unr.teamdynamix.com/TDClient/Requests/ServiceDet?ID=27661').catch((err) =>
+          console.error('Error opening URL:', err)
+        );
+      } else {
+        // Navigate to screen if no URL
+        navigation.navigate(screen);
+      }
+    }}
   >
-
-   <Image 
-     style = {styles.image} 
-     source = {{uri: path}} 
-     onError={(error) => console.log('Image loading error:', error)}
-   />
-     
-   <Text 
-     style={{color: '#fff', fontSize: 20,}}
-     adjustsFontSizeToFit={true}
-     allowFontScaling={true}
-   >
-   {title}
-   </Text>
-
- </TouchableOpacity>
+    <Image style={styles.image} source={path} onError={(error) => console.log('Image loading error:', error)} />
+    <Text style={{ color: '#fff', fontSize: 20 }} adjustsFontSizeToFit={true} allowFontScaling={true}>
+      {title}
+    </Text>
+  </TouchableOpacity>
 );
 
 function Header() {
