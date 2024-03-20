2dac441bc16013499da010ab803e91001c29e5f7
Buslon_Brian <buslon.aact@gmail.com>
2024-03-20 14:46:00 -0700

    Added login post to sheets

--- routes/LoginScreen.js
+++ routes/LoginScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {View, Text, TextInput, TouchableOpacity,} from 'react-native';
 import { store } from '../stores/store';
 import styles from '../components/styles';
@@ -6,31 +6,87 @@ import styles from '../components/styles';
 function LoginScreen({ navigation }) {
   
   const [text, onChangeText] = React.useState('');
+  const [error, setError] = useState(false);
   
-  return (
-    <View style={styles.container}>
+  if(error == false){
+    return (
+      <View style={styles.container}>
+    
+        <Text style = {{color: '#fff', fontSize: 25, marginBottom: 15}}>Welcome</Text>
+        <Text style = {{color: '#fff', fontSize: 17, marginBottom: 11}}>Please enter your first and last name:</Text>
+        
+        <TextInput 
+        style = {styles.searchbox} 
+        placeholder= "Enter your name..." 
+        placeholderTextColor = '#5c5c5c'
+        onChangeText={onChangeText}
+        value = {text}
+        onSubmitEditing={()=> validate(text, navigation, setError)}
+        />
+    
+        <TouchableOpacity 
+          style = {{backgroundColor: '#333940', margin: 15, paddingVertical:10, paddingHorizontal: 55, alignItems: 'center', marginTop: 35}} 
+          onPress={() => validate(text, navigation, setError)} 
+        >
+          <Text style = {{color: '#fff', fontSize: 16}}> Continue </Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
+  else {
+    return (
+      <View style={styles.container}>
+    
+        <Text style = {{color: '#fff', fontSize: 25, marginBottom: 15}}>Welcome</Text>
+        <Text style = {{color: '#fff', fontSize: 17, marginBottom: 11}}>Please enter your first and last name:</Text>
+        
+        <TextInput 
+        style = {styles.searchbox} 
+        placeholder= "Enter your name..." 
+        placeholderTextColor = '#5c5c5c'
+        onChangeText={onChangeText}
+        value = {text}
+        onSubmitEditing={()=> validate(text, navigation, setError)}
+        />
+        
+        <Text style = {{color: 'red', fontSize:17}}> Error: Invalid firstname or lastname</Text>
+        
+        <TouchableOpacity 
+          style = {{backgroundColor: '#333940', margin: 15, paddingVertical:10, paddingHorizontal: 55, alignItems: 'center', marginTop: 35}} 
+          onPress={() => validate(text, navigation, setError)} 
+        >
+          <Text style = {{color: '#fff', fontSize: 16}}> Continue </Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
   
-      <Text style = {{color: '#fff', fontSize: 25, marginBottom: 15}}>Welcome</Text>
-      <Text style = {{color: '#fff', fontSize: 17, marginBottom: 11}}>Please enter your first and last name:</Text>
-      
-      <TextInput 
-      style = {styles.searchbox} 
-      placeholder= "Enter your name..." 
-      placeholderTextColor = '#5c5c5c'
-      onChangeText={onChangeText}
-      value = {text}
-      onSubmitEditing={()=> store.username = text}
-      />
-  
-      <TouchableOpacity 
-        style = {{backgroundColor: '#333940', margin: 15, paddingVertical:10, paddingHorizontal: 55, alignItems: 'center', marginTop: 35}} 
-        onPress={() => navigation.navigate('Keys')}
-      >
-        <Text style = {{color: '#fff', fontSize: 16}}> Continue </Text>
-      </TouchableOpacity>
-    </View>
-  );
 }
 
+async function validate(input, navigation, setError){
+  store.username = input
+  let firstname = input.split(" ")[0]
+  let lastname = input.split(" ")[1]
+
+  const res = await fetch(process.env.EXPO_PUBLIC_SHEETSURL, {
+    method: "POST",
+    
+    body: JSON.stringify({
+      'scriptfn': 'verify_user',
+      'firstname': firstname,
+      'lastname': lastname,
+    })
+    
+  })
+  
+  const output = await res.text()
+  if(output == "true"){
+  setError(false)  
+  navigation.navigate('Keys')
+  }
+
+  else{setError(true)}
+}
 
 export default LoginScreen
